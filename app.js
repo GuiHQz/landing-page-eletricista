ae4d47365ab630c120c464e34019461d4b4d19fe
GuiHQz <guilher.queiroz@gmail.com>
2024-06-12 18:40:59 -0300

    feat: Add nova funcionalidade app.js

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
+const navLinks = document.querySelectorAll(".nav-list a");
 
 hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+
+// Adiciona um evento de clique a cada link de navegação para fechar o menu
+navLinks.forEach(link => {
+  link.addEventListener("click", () => nav.classList.remove("active"));
+});
