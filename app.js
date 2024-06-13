f432b342cbc342437161648a2f2c1fc73c2360ee
GuiHQz <guilher.queiroz@gmail.com>
2024-06-13 16:34:28 -0300

    feat: Add correção de estilo mobile

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const navLinks = document.querySelectorAll(".nav-list a");
 
 hamburger.addEventListener("click", () => nav.classList.toggle("active"));
 
-// Adiciona um evento de clique a cada link de navegação para fechar o menu
 navLinks.forEach(link => {
   link.addEventListener("click", () => nav.classList.remove("active"));
 });
